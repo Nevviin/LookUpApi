953c0ea6d3ffb50eff7bfb3fb5a7a38ae794c22c
Nevin <nevviin@gmail.com>
2021-04-24 00:03:22 +1000

    adding the controller and logging framework

---
+++ LookUp/LookUp.Api/Controllers/CandidateController.cs
@@ -0,0 +1,31 @@
+﻿using LookUp.Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LookUp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CandidateController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        public CandidateController(ILogger<CandidateController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Produces("application/json")]
+        public ActionResult<Candidate> GetCandidate()
+        {
+            //_logger.LogInformation("Candidate Get Executeing");
+            return Ok(new Candidate { Name = "test", Phone = "test" });
+            
+        }
+
+    }
+}
---
+++ LookUp/LookUp.Api/Models/Candidate.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LookUp.Api.Models
+{
+    public class Candidate
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("phone")]
+        public string Phone { get; set; }
+    }
+}
