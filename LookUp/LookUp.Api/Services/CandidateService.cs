144d0a26aaadd51d8234812499745fa17001aafe
Nevin <nevviin@gmail.com>
2021-04-24 11:36:18 +1000

    Serilog nuget installation and configuration in appsettings candidate service

---
+++ LookUp/LookUp.Api/Services/CandidateService.cs
@@ -0,0 +1,13 @@
+﻿using LookUp.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LookUp.Api.Services
+{
+    public class CandidateService : ICandidateService
+    {
+        public Candidate GetDefaultCandidate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
